9fec3da45506f8e75833ef8bbbc1eb7902638740
aless <alessandrina.deloffre@gmail.com>
2020-12-06 19:04:07 +0100

    first tests on cards classes

--- src/tarot/Tarot.java
+++ src/tarot/Tarot.java
@@ -1,10 +1,21 @@
 package tarot;
 
-public class tarot {
+public class Tarot {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Card card = new Card(new CardMajor().deck, new CardMinor().deck);
+		//card.generateDeck(new CardMajor().deck, new CardMinor().deck);
+		card.shuffleDeck(card.deck);
+		for(int i=0; i<card.deck.length;i++) {
+			System.out.println(card.deck[i].toString());
+		}
+		for(int i=0; i<card.deckMajor.length;i++) {
+			System.out.println(card.deckMajor[i].toString());
+		}
+		
 	}
 
 }
